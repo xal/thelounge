86f4424cbc6d3cd6daaa97feb2ebf53a6e3b040c
Richard Lewis <richrd.lewis@gmail.com>
2019-10-25 21:37:40 +0000

    Implement switchToChannel method.

--- client/components/InlineChannel.vue
+++ client/components/InlineChannel.vue
@@ -16,7 +16,7 @@ export default {
 			);
 
 			if (existingChannel) {
-				this.$router.push("chan-" + existingChannel.id);
+				this.$root.switchToChannel(existingChannel);
 			}
 
 			// TODO: Required here because it breaks tests
